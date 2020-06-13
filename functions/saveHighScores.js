24eba15b5deb013b1f1ae1b94b3b72ffe6c27564
AcarMeel <acarmeel@gmail.com>
2020-06-13 17:48:47 -0600

    Save

---
+++ functions/saveHighScores.js
@@ -0,0 +1,72 @@
+require('dotenv').config()
+
+const Airtable = require('airtable')
+
+
+Airtable.configure({
+    apiKey: process.env.AIRTABLE_API_KEY
+});
+
+const base = Airtable.base(process.env.AIRTABLE_API_BASE);
+const table = base.table(process.env.AIRTABLE_TABLE_NAME);
+
+
+exports.handler = async (event) => {
+    try {
+        if (event.httpMethod !== 'POST') {
+            return {
+                statusCode: 405,
+                body: JSON.stringify({
+                    msg: 'Method Not Allowed'
+                })
+            }
+        }
+        const { score, name } = JSON.parse(event.body);
+        if (typeof score === 'undefined' || !name) {
+            return {
+                statusCode: 405,
+                body: JSON.stringify({ err: 'Bad request' }),
+            };
+        }
+
+        try {
+            const records = await table
+                .select({
+                    sort: [{ field: 'score', direction: 'desc' }],
+                })
+                .firstPage();
+            const formattedRecords = records.map((record) => ({
+                id: record.id,
+                fields: record.fields,
+            }));
+
+            const lowestRecord = formattedRecords[9];
+            if (
+                typeof lowestRecord.fields.score === 'undefined' ||
+                score > lowestRecord.fields.score
+            ) {
+                //update this record with the incoming score
+                const updatedRecord = {
+                    id: lowestRecord.id,
+                    fields: { name, score },
+                };
+                await table.update([updatedRecord]);
+                return {
+                    statusCode: 200,
+                    body: JSON.stringify(updatedRecord),
+                };
+            } else {
+                return {
+                    statusCode: 200,
+                    body: JSON.stringify({}),
+                };
+            }
+        } catch (error) {
+            return {
+                statusCode: 500,
+                body: JSON.stringify({
+                    msg: error
+                })
+            }
+        }
+    }
