582836fd5c79a4e6a322495baf6006dd678f424a
AcarMeel <acarmeel@gmail.com>
2020-06-13 22:17:07 -0600

    Auth0

--- functions/saveHighScores.js
+++ functions/saveHighScores.js
@@ -1,65 +1,65 @@
 const { table, getHighScores } = require('./utils/airtable');
-const { getAccessTokenFromHeaders } = require('./utils/auth');
-
+const {
+    getAccessTokenFromHeaders,
+    validateAccessToken,
+} = require('./utils/auth');
 exports.handler = async (event) => {
     const token = getAccessTokenFromHeaders(event.headers);
-    if (!token) {
+    const user = await validateAccessToken(token);
+    if (!user) {
         return {
-            statusCode: 401,
-            body: JSON.stringify({ err: 'User is not logged in' }),
+            statusCode: 403,
+            body: JSON.stringify({ err: 'Unauthorized' }),
         };
     }
-        if (event.httpMethod !== 'POST') {
+
+    if (event.httpMethod !== 'POST') {
+        return {
+            statusCode: 405,
+            body: JSON.stringify({ err: 'That method is not allowed' }),
+        };
+    }
+
+    const { score } = JSON.parse(event.body);
+    const name = user['https://learnbuildtype/username'];
+    if (typeof score === 'undefined' || !name) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ err: 'Bad request' }),
+        };
+    }
+
+    try {
+        const records = await getHighScores(false);
+
+        const lowestRecord = records[9];
+        if (
+            typeof lowestRecord.fields.score === 'undefined' ||
+            score > lowestRecord.fields.score
+        ) {
+            //update this record with the incoming score
+            const updatedRecord = {
+                id: lowestRecord.id,
+                fields: { name, score },
+            };
+            await table.update([updatedRecord]);
             return {
-                statusCode: 405,
-                body: JSON.stringify({
-                    msg: 'Method Not Allowed'
-                })
-            }
-        }
-        const { score, name } = JSON.parse(event.body);
-        if (typeof score === 'undefined' || !name) {
+                statusCode: 200,
+                body: JSON.stringify(updatedRecord),
+            };
+        } else {
             return {
-                statusCode: 405,
-                body: JSON.stringify({ err: 'Bad request' }),
+                statusCode: 200,
+                body: JSON.stringify({}),
             };
         }
-
-        try {
-            const records = await getHighScores(false);
-            const lowestRecord = records[9];
-            if (typeof lowestRecord === 'undefined') {
-                return {
-                    statusCode: 200,
-                    body: JSON.stringify({}),
-                };
-            }
-            if (
-                typeof lowestRecord.fields.score === 'undefined' ||
-                score > lowestRecord.fields.score
-            ) {
-                //update this record with the incoming score
-                const updatedRecord = {
-                    id: lowestRecord.id,
-                    fields: { name, score },
-                };
-                await table.update([updatedRecord]);
-                return {
-                    statusCode: 200,
-                    body: JSON.stringify(updatedRecord),
-                };
-            } else {
-                return {
-                    statusCode: 200,
-                    body: JSON.stringify({}),
-                };
-            }
-        } catch (error) {
-            return {
-                statusCode: 500,
-                body: JSON.stringify({
-                    msg: error
-                })
-            }
-        }
+    } catch (err) {
+        console.error(err);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                err: 'Failed to save score in Airtabl',
+            }),
+        };
     }
+};
